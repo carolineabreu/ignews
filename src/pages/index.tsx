da6fe7dda42c0f7fb809b12a3e78f5579abcbda3
Caroline Abreu <carols_a@live.com>
2023-04-20 16:25:13 -0300

    Home page test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import { stripe } from "../services/stripe";
 interface HomeProps {
   product: {
     priceId: string;
-    amount: number;
+    amount: string;
   };
 }
 
@@ -28,7 +28,7 @@ export default function Home({ product }: HomeProps) {
             Get acess to all publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
 
         {/* eslint-disable-next-line @next/next/no-img-element */}
