8127deab30ae5d663238ca9789092fb275c0f769
Caroline Abreu <carols_a@live.com>
2023-04-20 15:12:53 -0300

    signInButton test

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,11 +7,13 @@ import styles from "./styles.module.scss";
 export function SignInButton() {
   const { data: session } = useSession();
 
+  console.log(session);
+
   return session ? (
-    <button type="button" className={styles.signInButton} onClick={() => signOut()}>
+    <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
       {session.user?.name}
-      <FiX color="#737380" className={styles.closeIcon} />
+      <FiX color="#737380" className={styles.closeIcon} onClick={() => signOut()} />
     </button>
   ) : (
     <button type="button" className={styles.signInButton} onClick={() => signIn("github")}>
